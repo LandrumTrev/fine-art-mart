32e8643108f979ea07e1dc2ed2e5490a37d4aab6
LandrumTrev <landrum.trevillian@gmail.com>
2018-10-24 18:47:07 -0400

    final choice work

--- customer.js
+++ customer.js
@@ -80,21 +80,73 @@ function buyersQuery(res) {
         ])
         .then(function (answer) {
 
+            var orderTotal = res[answer.item].retail_price * answer.quantity;
+
             console.log("\nYou selected " + chalk.yellow(answer.quantity) + " copies of " + chalk.yellow(res[answer.item].product_name) + " by " + chalk.greenBright(res[answer.item].artist_name));
 
-            if ( answer.quantity > res[answer.item].stock_quantity ) {
+            if (answer.quantity > res[answer.item].stock_quantity) {
 
                 console.log("\nWe're sorry, but we only have " + chalk.yellow(res[answer.item].stock_quantity) + " copies of " + chalk.yellow(res[answer.item].product_name) + " by " + chalk.greenBright(res[answer.item].artist_name) + " in stock right now. \nPlease enter your desired item number again and choose a quantity less than " + chalk.yellow(res[answer.item].stock_quantity) + ".\n");
 
                 buyersQuery(res);
 
             } else {
-                
-                console.log(chalk.blue("\nExcellent choice.") + " Your total is " + chalk.yellow("$" + res[answer.item].retail_price * answer.quantity) + " (" + answer.quantity + " @ $" + res[answer.item].retail_price + " each)\n");
 
-                console.log(chalk.blue("\nPlease enter the number of one of the following choices: \n") + chalk.yellow("1. [PLACE ORDER]") + " to complete your transaction, \n" + chalk.yellow("2. [EDIT SELECTION]") + " to change your selection and/or quantity, or \n" + chalk.yellow("3. [EXIT]") + " if you are not ready to order right now.\n");
+                console.log(chalk.blue("\nExcellent choice.") + " Your total is " + chalk.yellow("$" + orderTotal) + " (" + answer.quantity + " @ $" + res[answer.item].retail_price + " each)\n");
+
+                inquirer
+                    .prompt({
+                        name: "finalchoice",
+                        type: "list",
+                        message: chalk.red("\nUse your up/down arrow keys to select one of the following: \n") + chalk.green("1. [PLACE ORDER]") + " to complete your transaction, \n" + chalk.green("2. [EDIT SELECTION]") + " to change your selection and/or quantity, or \n" + chalk.green("3. [EXIT]") + " if you are not ready to order right now.\n",
+                        choices: ["PLACE ORDER", "EDIT SELECTION", "EXIT"],
+
+                    })
+                    .then(function (answer, orderTotal) {
+
+                        // REWRITE SWITCH CASE AS IF ELSE
+                        
+                        // switch (answer.action) {
+                        //     case "PLACE ORDER":
+                        //         console.log(orderTotal);
+                        //         console.log("Monkey Butts!");
+                        //         placeOrder(res, answer, orderTotal);
+                        //         break;
+
+                        //     case "EDIT SELECTION":
+                        //         buyersQuery();
+                        //         break;
+
+                        //     case "EXIT":
+                        //         userExit();
+                        //         break;
+
+                        //     // default:
+                        //     //     placeOrder();
+                        // }
+
+
+                    });
 
             }
 
+
+            function placeOrder(res, answer, orderTotal) {
+
+                // call the database and subtract the order quantity from the items stock_quanity
+
+                console.log("\nThank you for your order. In case you need more copies of THE_ITEM, we now currently have NEW_QUANTITY THE_ITEM prints in stock.\n");
+
+                // console.log("Thank you for your order. Your account has been charged " + chalk.yellow("$" + orderTotal));
+
+            };
+
+            function userExit() {
+
+                console.log("Thank you for visiting Fine Art Mart. Please come again soon.");
+
+            };
+
         });
+
 }
